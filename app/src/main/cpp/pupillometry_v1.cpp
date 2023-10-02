c30957fd3c8d7130a0ed0ca2a77d43892e003e7f
Reuben Hellier <reuben.hellier@student.manchester.ac.uk>
2023-10-02 12:23:46 +0100

    Edited ProcessDataActivity to now process data without saving image files. (Old method still in place for debugging purposes)

--- app/src/main/cpp/pupillometry_v1.cpp
+++ app/src/main/cpp/pupillometry_v1.cpp
@@ -15,34 +15,17 @@ using namespace std;
 using namespace cv;
 
 extern "C" JNIEXPORT jstring JNICALL
-Java_com_example_pupillometry_1v1_ProcessDataActivity_processImage(JNIEnv *env, jobject thiz, jstring imagePath, jstring resultPath) {
-    const char *imagePathStr = env->GetStringUTFChars(imagePath, nullptr);
-    const char *resultPathStr = env->GetStringUTFChars(resultPath, nullptr);
+Java_com_example_pupillometry_1v1_ProcessDataActivity_processImage(JNIEnv *env, jobject thiz, jlong image) {
+    Mat &img = *(Mat *) image;
 
-    Mat img, gray, debug;
+    Mat gray, debug;
 
     // Read image in both color and grayscale
-    img = imread(imagePathStr, IMREAD_COLOR);
-    if (img.empty()) {
-        __android_log_print(ANDROID_LOG_DEBUG, "C++", "Image empty :(");
-        exit(-1);
-    } else {
-        __android_log_print(ANDROID_LOG_DEBUG, "C++", "Image loaded :)");
-    }
     cvtColor(img, gray, COLOR_BGR2GRAY);
 
     // Run the detector
     pure::Detector detector;
     auto result = detector.detect(gray, &debug);
-
-    // Draw result on color image
-    ellipse(img, Point(result.center), Size(result.axes), result.angle, 0, 360, Scalar(0, 0, 255));
-
-    // Save image
-    imwrite(resultPathStr, img);
-
-    env->ReleaseStringUTFChars(imagePath, imagePathStr);
-    env->ReleaseStringUTFChars(resultPath, resultPathStr);
 
     std::string center_x = std::to_string(result.center.x);
     std::string center_y = std::to_string(result.center.y);
@@ -55,8 +38,45 @@ Java_com_example_pupillometry_1v1_ProcessDataActivity_processImage(JNIEnv *env, 
     std::string confidence_outline_contrast = std::to_string(result.confidence.outline_contrast);
 
     std::string res = center_x + "#" + center_y + "#" + width + "#" + height + "#" + angle
-            + "#" + confidence_value + "#" + confidence_aspect_ratio + "#"
-            + confidence_angular_spread + "#" + confidence_outline_contrast;
+                      + "#" + confidence_value + "#" + confidence_aspect_ratio + "#"
+                      + confidence_angular_spread + "#" + confidence_outline_contrast;
+
+    return env->NewStringUTF(res.c_str());
+}
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_example_pupillometry_1v1_ProcessDataActivity_processImageWithSave(JNIEnv *env, jobject thiz, jlong image, jstring resultPath) {
+    Mat &img = *(Mat *) image;
+    const char *resultPathStr = env->GetStringUTFChars(resultPath, nullptr);
+
+    Mat gray, debug;
+
+    // Read image in both color and grayscale
+    cvtColor(img, gray, COLOR_BGR2GRAY);
+
+    // Run the detector
+    pure::Detector detector;
+    auto result = detector.detect(gray, &debug);
+
+    // Draw ellipse
+    ellipse(img, Point(result.center), Size(result.axes), result.angle, 0, 360, Scalar(0, 0, 255));
+
+    // Save image
+    imwrite(resultPathStr, img);
+
+    std::string center_x = std::to_string(result.center.x);
+    std::string center_y = std::to_string(result.center.y);
+    std::string width = std::to_string(result.axes.width);
+    std::string height = std::to_string(result.axes.height);
+    std::string angle = std::to_string(result.angle);
+    std::string confidence_value = std::to_string(result.confidence.value);
+    std::string confidence_aspect_ratio = std::to_string(result.confidence.aspect_ratio);
+    std::string confidence_angular_spread = std::to_string(result.confidence.angular_spread);
+    std::string confidence_outline_contrast = std::to_string(result.confidence.outline_contrast);
+
+    std::string res = center_x + "#" + center_y + "#" + width + "#" + height + "#" + angle
+                      + "#" + confidence_value + "#" + confidence_aspect_ratio + "#"
+                      + confidence_angular_spread + "#" + confidence_outline_contrast;
 
     return env->NewStringUTF(res.c_str());
 }
--- app/src/main/java/com/example/pupillometry_v1/ProcessDataActivity.java
+++ app/src/main/java/com/example/pupillometry_v1/ProcessDataActivity.java
@@ -12,6 +12,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.opencsv.CSVWriter;
 
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.video.Video;
+import org.opencv.videoio.VideoCapture;
+import org.opencv.videoio.Videoio;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
@@ -53,6 +60,67 @@ public class ProcessDataActivity extends AppCompatActivity {
         Log.d("frameExtraction", "Beginning frame extraction");
         String videoFilePath = Environment.getExternalStorageDirectory() + "/Documents/Pupillometry/video.mp4";
 
+        // Initialise the MetadataRetriever and other variables
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+        retriever.setDataSource(videoFilePath);
+
+        String duration = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
+        long videoDuration = Long.parseLong(duration);
+        int frameInterval = 33;
+
+        // Initialise objects to contain data for the csv
+        String csvFilePath = Environment.getExternalStorageDirectory() + "/Documents/Pupillometry/data.csv";
+        List<String[]> data = new ArrayList<>();
+        data.add(new String[]{"frame", "center_x", "center_y", "width", "height", "angle",
+                "confidence_value", "confidence_aspect_ratio", "confidence_angular_spread",
+                "confidence_outline_contrast"});
+
+        // Loop through every frame in the video
+        int frameNumber = 0;
+        for (long time = 0; time < videoDuration; time += frameInterval) {
+            // Retrieve a bitmap of the frame
+            Bitmap bmp = retriever.getFrameAtTime(time * 1000, MediaMetadataRetriever.OPTION_CLOSEST);
+
+            // Convert this to an openCV matrix
+            Mat mat = new Mat();
+            Utils.bitmapToMat(bmp, mat);
+
+            // Keep the user updated
+            int currFrameNumber = frameNumber;
+            String frameCountText = "Frame: " + currFrameNumber;
+            runOnUiThread(() -> frameCount.setText(frameCountText));
+
+            // Call to PURE algorithm
+            String cppRes = processImage(mat.getNativeObjAddr());
+//            String resultPath = Environment.getExternalStorageDirectory() + "/Documents/Pupillometry/Frames/" + String.format(Locale.ENGLISH, "%06d", frameNumber) + "_res.jpeg";
+//            String cppRes = processImageWithSave(mat.getNativeObjAddr(), resultPath);
+
+            // Add data to array to later save as CSV
+            String res = String.format(Locale.ENGLISH, "%06d", frameNumber) + "#" + cppRes;
+            String[] row = res.split("#");
+            data.add(row);
+
+            frameNumber++;
+        }
+
+        // Save the data to a csv
+        try {
+            CSVWriter csvWriter = new CSVWriter(new FileWriter(csvFilePath));
+            csvWriter.writeAll(data);
+            csvWriter.close();
+        } catch (IOException e) {
+            Log.d("CSVWriter", "IOException thrown at new FileWriter()");
+            e.printStackTrace();
+        }
+
+        runOnUiThread(() -> frameCount.setText(R.string.completed_processing));
+        Log.d("frameExtraction", "Frame extraction completed");
+    }
+
+    private void processDataDeprecated() {
+        Log.d("frameExtraction", "Beginning frame extraction");
+        String videoFilePath = Environment.getExternalStorageDirectory() + "/Documents/Pupillometry/video.mp4";
+
         MediaMetadataRetriever retriever = new MediaMetadataRetriever();
         retriever.setDataSource(videoFilePath);
 
@@ -70,10 +138,10 @@ public class ProcessDataActivity extends AppCompatActivity {
         int frameNumber = 0;
         for (long time = 0; time < videoDuration; time += frameInterval) {
             Bitmap frame = retriever.getFrameAtTime(time * 1000, MediaMetadataRetriever.OPTION_CLOSEST);
-            int finalFrameNumber = frameNumber;
+            int currFrameNumber = frameNumber;
 
             // Keep the user updated
-            String frameCountText = "Frame: " + finalFrameNumber;
+            String frameCountText = "Frame: " + currFrameNumber;
             runOnUiThread(() -> frameCount.setText(frameCountText));
 
             String frameNumberFormatted = String.format(Locale.ENGLISH, "%06d", frameNumber);
@@ -89,10 +157,13 @@ public class ProcessDataActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
 
+            Imgcodecs imgcodecs = new Imgcodecs();
             String imagePath = Environment.getExternalStorageDirectory() + "/Documents/Pupillometry/Frames/" + frameNumberFormatted + ".jpeg";
+            Mat mat = imgcodecs.imread(imagePath);
+
             String resultPath = Environment.getExternalStorageDirectory() + "/Documents/Pupillometry/Frames/" + frameNumberFormatted + "_res.jpeg";
-            String cppRes = processImage(imagePath, resultPath);
-            String res = frameNumberFormatted+ "#" + cppRes;
+            String cppRes = processImageWithSave(mat.getNativeObjAddr(), resultPath);
+            String res = frameNumberFormatted + "#" + cppRes;
 
             String[] row = res.split("#");
             data.add(row);
@@ -113,5 +184,6 @@ public class ProcessDataActivity extends AppCompatActivity {
         Log.d("frameExtraction", "Frame extraction completed");
     }
 
-    public native String processImage(String imagePath, String resultPath);
+    public native String processImageWithSave(long image, String resultPath);
+    public native String processImage(long image);
 }
